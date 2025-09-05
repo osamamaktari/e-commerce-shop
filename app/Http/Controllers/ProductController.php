b5928c6cef503ce9686fea688c4df9622fb71861
osamamaktari <osamaaltal01@gmail.com>
2025-09-05 23:10:05 +0300

    feat: Implement Read (list & single product) functionality

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function index() {
+    $products = Product::all();
+    return view('shop.products', compact('products'));
+}
+
+public function show($id) {
+    $product = Product::findOrFail($id);
+    return view('shop.product-details', compact('product'));
+}
+
 }
--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,8 @@ Route::get('/about', [StoreController::class, 'about'])->name('about');
 Route::get('/contact', [StoreController::class, 'contact'])->name('contact');
 Route::get('/cart', [StoreController::class, 'cart'])->name('cart');
 Route::get('/register', [StoreController::class, 'register'])->name('register');
+
+
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
+
